7e028e49cb543ed9f7913512e102bbc61b78e3c8
Resti Ramadhani <restiramdhani30@gmail.com>
2025-05-24 13:28:03 +0700

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,8 @@ import { createRoot } from 'react-dom/client';
 import "bulma/css/bulma.css";
 import App from './App.js';
 
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
