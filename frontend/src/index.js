f39a5622670112522e45a0f90796b3ace62a59ea
restirama <163100778+restirama147@users.noreply.github.com>
2025-05-24 14:43:00 +0700

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import "bulma/css/bulma.css";
 import App from './App.js';
+import { AuthProvider } from './auth/AuthProvider.js'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
